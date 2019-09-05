1ba33945b8caf3ee1e19ba02a7346dc3730875ee
Diogo Gomes <dgomes@ua.pt>
2019-09-05 11:44:34 +0100

    add consts

--- consts.py
+++ consts.py
@@ -9,3 +9,14 @@ class Powerups(Enum):
     Bombpass = 6,
     Flamepass = 7,
     Mystery = 8  
+
+class Speed(IntEnum):
+    SLOWEST = 1,
+    SLOW = 2,
+    NORMAL = 3,
+    FAST = 4
+
+class Smart(IntEnum):
+    LOW = 1,
+    NORMAL = 2,
+    HIGH = 3
